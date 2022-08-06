3dbe9dc32aef4cdaf102409d79154eaad9800aaa
Jonathan Grundy <jonat@macbook-air.lan>
2022-08-06 17:57:15 -0400

    main loop added in movie_analysis_cleaning_jg, added tentative text cleaning function"

--- cleaning_funcs.py
+++ cleaning_funcs.py
@@ -4,8 +4,19 @@ import os
 import spacy as sp
 
 def load_data(file):
+    """
+    Imports data from a csv file at a relative path.
+    """
     dirname = os.path.dirname(os.path.abspath("__file__"))
     filename = os.path.join(dirname, 'data/'+file)
     df = pd.read_csv(filename)
     return df
 
+def text_preprocessing(text):
+    """
+    Preprocesses text by removing punctuation, lowercasing, and tokenizing.
+    """
+    nlp = sp.load('en_core_web_sm')
+    doc = nlp(text)
+    tokens = [token.text for token in doc]
+    return tokens
